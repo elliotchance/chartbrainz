976fd5a08428ddd6da3fde8174cb32a97e5d14c1
Elliot Chance <elliotchance@gmail.com>
2022-11-22 01:19:21 -0500

    Bug fixes (#19)
    1. Fixed a bug with the logout where it might not clear all cookies in
    case of an error.
    2. Fixed a bug where inline ratings change from a search would not show
    the new rating value.

--- handlers/logout.js
+++ handlers/logout.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 module.exports.handler = (event, context, callback) => {
   let accessToken = "";
-  for (const cookie of event.multiValueHeaders.cookie || []) {
+  for (const cookie of event.multiValueHeaders?.cookie || []) {
     if (cookie.indexOf("bearer=") >= 0) {
       accessToken = cookie.split(";")[0].split("=")[1];
     }
@@ -26,19 +26,16 @@ module.exports.handler = (event, context, callback) => {
       )}&client_secret=${encodeURIComponent(clientSecret)}`,
     },
     function (error) {
-      if (error) {
-        callback(error);
-        return;
-      }
-
+      // We should never return an error, even if something goes wrong wipe out
+      // the cookies.
       callback(null, {
         statusCode: 302,
         multiValueHeaders: {
           Location: [domain],
           "Set-Cookie": [
-            `bearer=;expires=Thu, 21 Sep 1979 00:00:01 UTC;`,
-            `refresh=;expires=Thu, 21 Sep 1979 00:00:01 UTC;`,
-            `user=;expires=Thu, 21 Sep 1979 00:00:01 UTC;`,
+            `bearer=; path=/; expires=Thu, 21 Sep 1979 00:00:01 UTC;`,
+            `refresh=; path=/; expires=Thu, 21 Sep 1979 00:00:01 UTC;`,
+            `user=; path=/; expires=Thu, 21 Sep 1979 00:00:01 UTC;`,
           ],
         },
       });
